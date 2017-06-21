bd650e428767812f979d9ffa86762b25158877d7
Jaydson Gomes <jayalemao@gmail.com>
2017-06-21 16:57:36 -0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
-if [[ git status -s ]]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
-fi
-
 echo "[ Deleting old publication ]"
 rm -rf public
 mkdir public
 
 echo "[ Generating Hugo website ]"
 hugo --theme=casper
-
-echo "[ Git commit ]"
-git add --all && git commit -m "Publishing to now"
 
 echo "[ Creating now alias config file ]"
 echo "{
